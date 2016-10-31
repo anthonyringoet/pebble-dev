92b71d04b59ac840cffdcc7193c122d40b5194fd
Anthony Ringoet <anthonyringoet@gmail.com>
2016-10-31 17:07:11 +0100

    Enable weather info

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,4 +1,42 @@
 // https://developer.pebble.com/docs/pebblekit-js/Pebble/#on
-Pebble.on('message', function (event) {
-  console.log('Message received from watch:', event.data)
+
+var myAPIKey = '91872865cea054f42192088d38e92f4d'
+
+function request(url, type, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.onload = function () {
+    callback(this.responseText)
+  }
+  xhr.open(type, url)
+  xhr.send()
+}
+
+Pebble.on('message', function(event) {
+  // Get the message that was passed
+  var message = event.data
+
+  if (message.fetch) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      var url = 'http://api.openweathermap.org/data/2.5/weather' +
+              '?lat=' + pos.coords.latitude +
+              '&lon=' + pos.coords.longitude +
+              '&appid=' + myAPIKey
+
+      request(url, 'GET', function(respText) {
+        var weatherData = JSON.parse(respText)
+
+        Pebble.postMessage({
+          'weather': {
+            // Convert from Kelvin
+            'celcius': Math.round(weatherData.main.temp - 273.15),
+            'fahrenheit': Math.round((weatherData.main.temp - 273.15) * 9 / 5 + 32),
+            'desc': weatherData.weather[0].main
+          }
+        })
+      })
+    }, function(err) {
+      console.error('Error getting location')
+    },
+    { timeout: 15000, maximumAge: 60000 })
+  }
 })
--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,4 +1,17 @@
 var rocky = require('rocky')
+var weather
+
+function drawWeather(ctx, weather) {
+  // Create a string describing the weather
+  var weatherString = weather.celcius + 'ºC, ' + weather.desc;
+  // var weatherString = weather.fahrenheit + 'ºF, ' + weather.desc
+
+  // Draw the text, top center
+  ctx.fillStyle = 'lightblue'
+  ctx.textAlign = 'center'
+  ctx.font = '14px Gothic'
+  ctx.fillText(weatherString, ctx.canvas.unobstructedWidth / 2, 2)
+}
 
 rocky.on('draw', function (event) {
   // Drawing canvas
@@ -21,6 +34,11 @@ rocky.on('draw', function (event) {
   ctx.fillStyle = 'white'
   ctx.textAlign = 'center'
 
+  // Draw the conditions (before clock hands, so it's drawn underneath them)
+  if (weather) {
+    drawWeather(ctx, weather);
+  }
+
   // Time
   // https://developer.pebble.com/docs/rockyjs/CanvasRenderingContext2D/#font
   ctx.font = '26px bold Leco-numbers-am-pm'
@@ -35,10 +53,28 @@ rocky.on('draw', function (event) {
   ctx.fillText(day + ' ' + month, centerX, (94 - offsetY))
 })
 
+rocky.on('message', function(event) {
+  // Receive a message from the mobile device (pkjs)
+  var message = event.data
+
+  if (message.weather) {
+    // Save the weather data
+    weather = message.weather
+
+    // Request a redraw so we see the information
+    rocky.requestDraw()
+  }
+})
+
 // Redraw every minute
 // https://developer.pebble.com/docs/rockyjs/rocky/#on
 rocky.on('minutechange', function (event) {
   rocky.requestDraw()
+})
+
+rocky.on('hourchange', function(event) {
+  // Send a message to fetch the weather information (on startup and every hour)
+  rocky.postMessage({'fetch': true})
 })
 
 // Send a single message to the Phone
